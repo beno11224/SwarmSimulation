fea30addc509abbf69fae339fe2013f2cc9bc0ac
beno11224 <beno11224@gmail.com>
2022-07-20 09:57:17 +0100

    Create pythontest.py

---
+++ pythontest.py
@@ -0,0 +1,23 @@
+from tensorflow import keras
+
+# TODO
+# Load in data
+
+
+# TODO
+# do keras training stuff
+
+# TODO
+# Save model
+
+
+
+# This will be a separate file at some point
+
+# TOOD
+# Load the file
+
+# TODO
+# run the keras model on matlab input
+
+#TODO return the output to matlab
