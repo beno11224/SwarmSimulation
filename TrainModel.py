26199ded5f1d61b25f756b051b52b02122366f41
beno11224 <beno11224@gmail.com>
2022-08-23 00:31:17 +0100

    training

--- TrainModel.py
+++ TrainModel.py
@@ -8,24 +8,57 @@ root = tk.Tk()
 root.withdraw()
 filez = fd.askopenfilenames(parent=root, title='Choose all data files')
     #print(filez)
+datax = []
+datay = []
 for fileName in filez:
     with open(fileName) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         lineCount = 0
         for row in csv_reader:
-            print(row[4])
-            lineCount += 1
-            #No, it's been read in cos python is better than matlab
-            #Now we read stuff
-            #datas = row.split(",");
-            #time = datas[1];
-            #magForce = datas[2];
-            #goalPercentage = datas[3];
-            #velocities = datas[4];
-            #positions = datas[5];
+
+            #TODO need to clean up the tuples.
             
+            #Make data easier to use
+            time = row[0]
+            magForce = row[1]
+            goalPercentage = row[2]
+            velocities = row[3]
+            positions = row[4]
+
+            splitPositions = []
+            splitMagForce = []
+            positions = positions.strip('[')
+            positions = positions.strip(']')
+            positions = positions.split(';')
+            for positionTuple in positions:
+                a = positionTuple.split(' ')
+                splitPositions.append(float(a[0]))
+                splitPositions.append(float(a[1]))
+               
+            datax.append(splitPositions)
+            
+            #only add the magforce for the next line
+            if lineCount == 0:
+                lineCount += 1
+                continue
+            
+            magForce = magForce.strip('[')
+            magForce = magForce.strip(']')
+            magForceboth = magForce.split(' ')
+           # datay.append(magForceboth)
+            #print(type(datay[0][0]))
+            splitMagForce.append(float(magForceboth[0]))
+            splitMagForce.append(float(magForceboth[1]))
+            datay.append(splitMagForce)
+           # datay.append(float(magForceboth[1]))
+          #  print(magForce)
+                  
+            lineCount += 1
+         #   print(lineCount)
+
         if lineCount == 0:
             print("No lines in file: " + fileName)
+        datax.pop(-1);
 
 #TODO sort out reading the polygons in.
 #polyLine = plot(ax1,polygon.currentPoly(:,1),polygon.currentPoly(:,2), 'Color','b');
@@ -44,18 +77,29 @@ for fileName in filez:
 #end
 
             
-datax = [[0.1],[0.8],[0.4]]
-datay = [[0.9],[0.1],[0.7]]
+#datax = [[0.1],[0.8],[0.4]]
+#datay = [[0.9],[0.1],[0.7]]
 
 # TODO
 # do keras training stuff
+print("Starting Training")
+#print(datax[0])
+#print(datay[0])
+#print(datax[2])
+#print(len(datax))
+#print(len(datax[0]))
 model = keras.models.Sequential()
-model.add(keras.layers.Dense(12, input_shape=(1,),activation='relu'))
-model.add(keras.layers.Dense(1, activation='sigmoid'))
+model.add(keras.layers.Dense(len(datax[0]), input_shape=(len(datax[0]),),activation='relu'))
+model.add(keras.layers.Dense(len(datay[0]), activation='sigmoid'))
+#model.add(keras.layers.Dense(12, input_shape=(1,),activation='relu'))
+#model.add(keras.layers.Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.fit(datax, datay, epochs=15, batch_size=2, verbose=0)
+print(model.summary())
+print("Fitting:")
+model.fit(datax, datay, epochs=15, batch_size=1, verbose=1)
+print("Fitted!")
 
-a = model.predict([0.8])
+a = model.predict(datax[:2])
 print(a)
 # TODO
 # Save model
