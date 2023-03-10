347605360a6c637588873144cfd95ce20885c21e
beno11224 <beno11224@gmail.com>
2023-03-10 11:27:02 +0000

    remove visual bounce

--- drawTimerCallback.m
+++ drawTimerCallback.m
@@ -4,10 +4,13 @@ function drawTimerCallback(app)
 
     rotatedMagLine = ([cosd(app.rotation), sind(app.rotation); -sind(app.rotation), cos(app.rotation)] * [0,app.X1MAGauge.Value./(app.maxForce.*75) ; 0,app.Y1MAGauge.Value./(app.maxForce.*75)])';
     app.magLine = plot(app.UIAxes,rotatedMagLine(:,1),rotatedMagLine(:,2),'magenta');
-
-    rotatedPoints = ([cosd(app.rotation), sind(app.rotation); -sind(app.rotation), cos(app.rotation)] * app.particleArrayLocation')';
+    
+    %a = app.bouncedLastLoop&app.bouncedVisualLastLoop
+    notBounced = app.particleArrayLocation((app.bouncedLastLoop&app.bouncedVisualLastLoop) == 0,:);
+    rotatedPoints = ([cosd(app.rotation), sind(app.rotation); -sind(app.rotation), cos(app.rotation)] * notBounced')';
+    %rotatedPoints = ([cosd(app.rotation), sind(app.rotation); -sind(app.rotation), cos(app.rotation)] * app.particleArrayLocation')';
     app.particlePoints = plot(app.UIAxes, app.particleFunctions.realNum(rotatedPoints(:,1)), app.particleFunctions.realNum(rotatedPoints(:,2)),'r.', 'markerSize', 23);
-   % plot(app.polygon.allPolys(2,:,1), app.polygon.allPolys(2,:,2));
+    % plot(app.polygon.allPolys(2,:,1), app.polygon.allPolys(2,:,2));
    % hold on
    % plot(app.polygon.allPolys(2,:,1), app.polygon.allPolys(2,:,2),'r.', 'markerSize', 13);
 
